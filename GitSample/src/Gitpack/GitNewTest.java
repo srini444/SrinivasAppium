cf1aaa4b7bef7f8c032caf3f66ea6adc0115f564
srini444 <srinivaschari.y@gmail.com>
2017-07-18 11:04:28 +0530

    Add My Blog Adders

---
+++ GitSample/src/Gitpack/GitNewTest.java
@@ -0,0 +1,26 @@
+package Gitpack;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class GitNewTest 
+{
+  
+  @BeforeTest
+  public void beforeTest() 
+  {
+	  
+  }
+  @Test
+  public void f() 
+  {
+	  
+  }
+  @AfterTest
+  public void afterTest() 
+  {
+	  
+  }
+
+}
